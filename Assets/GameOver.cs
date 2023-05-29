1d7164ba1d1063cf9dea04242ffab57fd535c35b
syun39 <134475243+syun39@users.noreply.github.com>
2023-05-29 22:11:51 +0900

    素材　　スクリプト　　タイトル　　

---
+++ Assets/GameOver.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField]
+    string _enemyTag = "Enemy";
+
+    int _gameover = 0;
+    
+
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == _enemyTag)
+        {
+            //_gameover++;
+            //Debug.Log(_gameover);
+            //if (_gameover == 1)
+            //{
+                
+            //}
+
+            ChangeScene();
+        }
+    }
+
+    void ChangeScene()
+    {
+         SceneManager.LoadScene("GameOver");
+    }
+}
