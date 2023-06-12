82a372b3e340ed037b3f9b8c39b8ba01fed9977d
syun39 <134475243+syun39@users.noreply.github.com>
2023-06-12 14:13:56 +0900

    ゲームクリアシーンとゲームオーバーシーンのボタンに効果音を付けた。 ゲームクリアシーンに拍手の効果音を追加

---
+++ Assets/Script/jumpoto.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class jumpoto : MonoBehaviour
+{
+   
+    public AudioClip _sound1;
+    AudioSource _SE;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _SE = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    //void Update()
+   void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            _SE.PlayOneShot(_sound1);
+        }
+    }
+}
