776f95d725c570426fedaa46ca36203e36292ddd
syun39 <134475243+syun39@users.noreply.github.com>
2023-06-25 18:10:17 +0900

    ウサギコントローラーを消したからウサギ移動、ウサギジャンプのスクリプトを追加

---
+++ Assets/Script/usagiidou.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class usagiidou : MonoBehaviour
+{
+    //‘¬“x
+    [SerializeField] float _speed = 5f;
+    private SpriteRenderer _renderer;
+    private Rigidbody2D _rigidbody2D;
+    void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontalKey = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey("left"))
+        {
+            _rigidbody2D.velocity = new Vector2(-_speed, _rigidbody2D.velocity.y);
+
+            _renderer.flipX = true;
+        }
+        else if (Input.GetKey("right"))
+        {
+            
+            _rigidbody2D.velocity = new Vector2(_speed, _rigidbody2D.velocity.y);
+            _renderer.flipX = false;
+        }
+        else
+        {
+            _rigidbody2D.velocity = Vector2.zero;
+        }
+       
+    }
+}
---
+++ Assets/Script/usagijump.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class usagijump : MonoBehaviour
+{
+    //ƒWƒƒƒ“ƒv‚·‚é—Í
+    [SerializeField] float _jumppower = 1f;
+    private Rigidbody2D _rigidbody2D;
+    int _jumpcount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && this._jumpcount < 1)
+        {
+            _rigidbody2D.AddForce(transform.up * _jumppower);
+            _jumpcount++;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _jumpcount = 0;
+        }
+    }
+}
