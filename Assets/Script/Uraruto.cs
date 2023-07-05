95027db16ce9754ac428b60d61d354b05fcf2c6c
syun39 <134475243+syun39@users.noreply.github.com>
2023-07-05 19:16:00 +0900

    イージーシーンに行くボタンとハードシーンに行くボタンが完成 裏ルートへの条件も完成　フォントを追加

---
+++ Assets/Script/Uraruto.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Uraruto : MonoBehaviour
+{
+    [SerializeField]
+    string _UrarutoTag = "Ura";
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == _UrarutoTag)
+        {
+            ChangeScene();
+        }
+
+    }
+    
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("Uraruto");
+
+    }
+}
