f73822b5795336b88beec65350fdd659bc810f31
syun39 <134475243+syun39@users.noreply.github.com>
2023-07-08 14:23:31 +0900

    難易度選択画面とイージーシーンとハードシーンにフェードインを追加

---
+++ Assets/Script/FadeIn.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Image>().enabled = true;
+    }
+
+   public void EndFadeInAnimation()
+    {
+        Destroy(this.gameObject);
+    }
+}
