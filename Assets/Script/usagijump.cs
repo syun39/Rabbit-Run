bab118e36771b150db1d63bb92848efd16abb520
syun39 <134475243+syun39@users.noreply.github.com>
2023-07-25 18:51:11 +0900

    熊追加　敵の熊追加　鹿追加　ウサギの接地判定を見直した　ハードシーンを難しくした

--- Assets/Script/usagijump.cs
+++ Assets/Script/usagijump.cs
@@ -37,8 +37,7 @@ public class UsagiJump : MonoBehaviour
             GetComponent<AudioSource>().Play();
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
- 
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
