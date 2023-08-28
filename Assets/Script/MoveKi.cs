2a4b7f9fd085d06fce84e1747897f1dfc86f3f89
syun39 <134475243+syun39@users.noreply.github.com>
2023-08-28 17:42:31 +0900

    動く木を増やした　罠の木も作った

--- Assets/Script/MoveKi.cs
+++ Assets/Script/MoveKi.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveKi : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
@@ -12,7 +12,7 @@ public class MoveKi : MonoBehaviour
             other.transform.SetParent(transform);
         }
     }
-    private void OnCollisionExit2D(Collision2D other)
+    private void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
