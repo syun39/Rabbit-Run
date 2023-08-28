1f7106f6e2ce702432d27795f85683e361f3d7e9
syun39 <134475243+syun39@users.noreply.github.com>
2023-08-28 16:07:52 +0900

    動く木を作った　木にプレイヤーが乗った時、木からプレイヤーが落ちないようにした

---
+++ Assets/Script/MoveKi.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveKi : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // G‚ê‚½obj‚Ìe‚ğˆÚ“®°‚É‚·‚é
+            other.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // G‚ê‚½obj‚Ìe‚ğ‚È‚­‚·
+            other.transform.SetParent(null);
+        }
+    }
+}
